2972beb4162a722ff033475d6812ddb37e4adf4f
Graham Hartle <graham@Grahams-iMac.gateway>
2018-07-22 09:33:44 +1000

    Added extra stuff to guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -6,10 +6,13 @@ points = 100
 
 print('What animal am I thinking about?')
 
-while guess != secret_word:
+while guess != secret_word and points >0:
     guess = input('Enter your guess: ')
     if guess != secret_word:
         points -= 10
 
-print('Weldone! You guessed correctly.')
-print(f'You scored {points} points')
+if points == 0:
+    print('You have not guessed correctly.')
+else:
+    print('Weldone! You guessed correctly.')
+    print(f'You scored {points} points')
