a76eb7e38328ea2b1778191c23ac68d0b0cbc561
Graham Hartle <graham@Grahams-iMac.gateway>
2018-07-22 09:56:51 +1000

    For loops

---
+++ for_loop.py
@@ -0,0 +1,12 @@
+# For loops
+
+# for letter in 'Graham Hartle':
+#     print(letter)
+
+# for num in range(5, 101, 5):
+#     print(num)
+
+friends = ['bob', 'jane', 'john', 'brian']
+
+for friend in friends:
+    print(friend.title())
