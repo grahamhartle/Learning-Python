260dd58a37a3e40edcdb0fc426b6b29d6094cb48
Graham Hartle <graham@Grahams-iMac.gateway>
2018-07-30 00:51:53 +1000

    Creating Windows with PyQt5

---
+++ PyQt5/pyqt2.py
@@ -0,0 +1,18 @@
+import sys
+from PyQt5 import QtWidgets, QtGui
+
+def window():
+    app = QtWidgets.QApplication(sys.argv)
+    w = QtWidgets.QWidget()
+    b = QtWidgets.QPushButton(w)
+    l = QtWidgets.QLabel(w)
+    b.setText('Push Me')
+    l.setText('Look at Me')
+    w.setWindowTitle('PyQt5 Lesson 2')
+    b.move(100, 50)
+    l.move(110, 100)
+    w.setGeometry(100, 100, 300, 200)
+    w.show()
+    sys.exit(app.exec_())
+
+window()
